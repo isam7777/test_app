c641d9eb76d34c37107edeb00c40c1ffc7e78d24
Moriki <62443569+isam7777@users.noreply.github.com>
2023-12-03 15:14:43 +0900

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  get '/' => "home#top"
+
+  get "about" => "home#about"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
